9cf0aa3a4588f9603b4e2c46193f082147667c9b
Stilyan Plamen Manolov <stilianmanolov05@gmail.com>
2023-12-19 13:57:25 +0200

    filters 100%

--- EcoBIke-Project/scr/JavaScript-files/Products.js
+++ EcoBIke-Project/scr/JavaScript-files/Products.js
@@ -1,10 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     const bikeList = document.querySelector('.featured-car-list');
     const bikes = Array.from(bikeList.children);
+    const titles = document.querySelectorAll('.title-Products-first, .title-Products');
+    const clearFiltersButton = document.getElementById('clear-filters-button');
 
     const sortLowToHighButton = document.getElementById('sort-low-to-high');
     const sortHighToLowButton = document.getElementById('sort-high-to-low');
     const yearFilterSelect = document.getElementById('year-filter');
+    const originalBikesOrder = Array.from(bikes);
+
+    const priceRanges = {
+        'range-0-400': [0, 400],
+        'range-400-600': [401, 599],
+        'range-600-800': [600, 800],
+    };
+      
+    const checkboxes = document.querySelectorAll('#price-ranges input[type="checkbox"]');
+    const resultsCount = document.getElementById('filtered-count');
 
     // Function to compare bike prices
     const comparePrices = (a, b) => {
@@ -14,6 +26,39 @@ document.addEventListener('DOMContentLoaded', function () {
         return priceA - priceB;
     };
 
+    // Function to update the price list
+      checkboxes.forEach((checkbox) => {
+        checkbox.addEventListener('change', updatePriceFilter);
+      });
+    
+      function updatePriceFilter() {
+        const selectedPriceRanges = [];
+    
+        checkboxes.forEach((checkbox) => {
+          if (checkbox.checked) {
+            const rangeId = checkbox.id;
+            selectedPriceRanges.push(priceRanges[rangeId]);
+          }
+        });
+    
+        if (selectedPriceRanges.length === 0) {   
+          return;
+        }
+    
+        const filteredBikes = bikes.filter((bike) => {
+          const bikePrice = parseFloat(bike.querySelector('.card-price strong').textContent.replace('$', ''));
+          for (const [minPrice, maxPrice] of selectedPriceRanges) {
+            if (bikePrice >= minPrice && bikePrice <= maxPrice) {
+              return true;
+            }
+          }
+          return false;
+        });
+    
+        updateBikeList(filteredBikes);
+        hideTitles();
+      }
+  
     // Function to update the bike list
     const updateBikeList = (bikeArray) => {
         bikeList.innerHTML = '';
@@ -22,14 +67,29 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     };
 
+    const hideTitles = () => {
+        titles.forEach((title) => {
+            title.style.display = 'none';
+        });
+    };
+
+    const showTitles = () => {
+        titles.forEach((title) => {
+            title.style.display = '';
+        });
+    };
+
     sortLowToHighButton.addEventListener('click', () => {
         bikes.sort(comparePrices);
         updateBikeList(bikes);
+        hideTitles();
     });
 
     sortHighToLowButton.addEventListener('click', () => {
+        bikes.sort(comparePrices);
         bikes.reverse();
         updateBikeList(bikes);
+        hideTitles();
     });
 
     // Event listener for year filter
@@ -45,5 +105,15 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         }
         updateBikeList(filteredBikes);
+        hideTitles();
+    });
+
+     // Event listener for clear filter
+    clearFiltersButton.addEventListener('click', () => {
+        yearFilterSelect.value = 'all';
+        bikes.length = 0; 
+        bikes.push(...originalBikesOrder);
+        updateBikeList(bikes);
+        showTitles();
     });
 });
