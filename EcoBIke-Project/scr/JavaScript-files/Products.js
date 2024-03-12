38ed76ab7932fc7a67eb45f97cb50fc5324abf16
Stilyan Plamen Manolov <stilianmanolov05@gmail.com>
2024-03-12 09:18:19 +0200

    bug fixes and better reading

--- EcoBIke-Project/scr/JavaScript-files/Products.js
+++ EcoBIke-Project/scr/JavaScript-files/Products.js
@@ -18,29 +18,7 @@ async function ProductsFirebase() {
   const db = getFirestore(app);
   const database = getDatabase(app, "https://ecobike-bb6cc-default-rtdb.europe-west1.firebasedatabase.app");
 
-  let bikes = [
-  ];
-
-  // Function to retrieve bikes data from Firebase and generate HTML
-  async function fetchBikes() {
-    const dbRef = ref(database, 'bikes');
-    try {
-      const snapshot = await get(dbRef);
-      if (snapshot.exists()) {
-        const bikesObj = snapshot.val();
-        bikes = Object.keys(bikesObj).map(key => ({
-          id: key,
-          ...bikesObj[key]
-        }));
-        renderBikes(bikes);
-        initFilters();
-      } else {
-        console.log("No data available");
-      }
-    } catch (error) {
-      console.error("Error fetching data: ", error);
-    }
-  }
+  let bikes = [];
 
 // Function to generate HTML for each bike
 function generateBikeHTML(bike) {
@@ -89,7 +67,27 @@ function generateBikeHTML(bike) {
     </div>
   `;
 }
-// End of HTML for each bike
+
+// Function to retrieve bikes data from Firebase and generate HTML
+async function fetchBikes() {
+  const dbRef = ref(database, 'bikes');
+  try {
+    const snapshot = await get(dbRef);
+    if (snapshot.exists()) {
+      const bikesObj = snapshot.val();
+      bikes = Object.keys(bikesObj).map(key => ({
+        id: key,
+        ...bikesObj[key]
+      }));
+      renderBikes(bikes);
+      initFilters();
+    } else {
+      console.log("No data available");
+    }
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+  }
+}
 
 // Function to render bikes on the page
 function renderBikes(bikesData) {
@@ -101,168 +99,15 @@ function renderBikes(bikesData) {
   });
 }
 
-fetchBikes();
-// End of function for rendering bikes
-
+// Function for calling all the functions
 async function loadDataAndCart() {
   await fetchBikes(); 
   fetchCartFromFirestore(currentUserId); 
+  initFilters();
 }
 loadDataAndCart();
+fetchBikes();
 
-//Filters
-function initFilters() {
-  const speedFilterSelect = document.getElementById('speed-filter');
-  const weightFilterSelect = document.getElementById('weight-filter');
-  const bikeList = document.querySelector('.featured-car-list');
-  const clearFiltersButton = document.getElementById('clear-filters-button');
-  const sortLowToHighButton = document.getElementById('sort-low-to-high');
-  const sortHighToLowButton = document.getElementById('sort-high-to-low');
-  const yearFilterSelect = document.getElementById('year-filter');
-  const checkboxes = document.querySelectorAll('#price-ranges input[type="checkbox"]');
-
-  const bikes = Array.from(bikeList.children);
-  const originalBikesOrder = Array.from(bikes);
-
-  // Price range for checkboxes
-  const priceRanges = {
-      'range-0-400': [0, 400],
-      'range-400-600': [401, 599],
-      'range-600-800': [600, 800],
-  };
-
-  // Compare bike prices
-  const comparePrices = (a, b) => {
-      const priceA = parseFloat(a.querySelector('.card-price strong').textContent.replace('$', ''));
-      const priceB = parseFloat(b.querySelector('.card-price strong').textContent.replace('$', ''));
-      return priceA - priceB;
-  };
-
-  // Update bike list
-  const updateBikeList = (bikeArray) => {
-    bikeList.innerHTML = ''; 
-    if (bikeArray.length > 0) {
-        bikeArray.forEach((bike) => {
-            bikeList.appendChild(bike);
-        });
-    } else {
-        bikeList.innerHTML = ' <img class = "notFound" src="https://firebasestorage.googleapis.com/v0/b/ecobike-bb6cc.appspot.com/o/itemNotFound.png?alt=media&token=e17013a0-ae20-48d1-ba37-6784199a0c64">';
-    }
-};
-
-// Function for working filters
-function filterBikes() {
-  const searchInput = document.getElementById('title-search').value.toLowerCase();
-  const selectedType = document.getElementById('type-filter').value;
-  
-  const selectedSpeedRange = speedFilterSelect.value;
-  let minSpeed = 0, maxSpeed = Infinity;
-  if (selectedSpeedRange !== 'all') {
-    [minSpeed, maxSpeed] = selectedSpeedRange.split('-').map(Number);
-  }
-
-  const selectedWeightRange = weightFilterSelect.value;
-  let minWeight = 0, maxWeight = Infinity;
-  if (selectedWeightRange !== 'all') {
-    [minWeight, maxWeight] = selectedWeightRange.split('-').map(Number);
-  }
-
-  const selectedYear = yearFilterSelect.value;
-  const selectedPriceRanges = [];
-  checkboxes.forEach((checkbox) => {
-    if (checkbox.checked) {
-      const rangeId = checkbox.id;
-      selectedPriceRanges.push(priceRanges[rangeId]);
-    }
-  });
-
-  const filteredBikes = originalBikesOrder.filter((bike) => {
-    
-    const bikePrice = parseFloat(bike.querySelector('.card-price strong').textContent.replace('$', ''));
-    const bikeYear = bike.querySelector('.year').getAttribute('value');
-    const bikeSpeed = parseFloat(bike.querySelector('.speed').textContent);
-    const isSpeedMatch = bikeSpeed >= minSpeed && bikeSpeed <= maxSpeed;
-    const isYearMatch = selectedYear === 'all' || bikeYear === selectedYear;
-    const isPriceMatch = selectedPriceRanges.length === 0 || selectedPriceRanges.some(([minPrice, maxPrice]) => bikePrice >= minPrice && bikePrice <= maxPrice);
-    const bikeWeight = parseFloat(bike.querySelector('.weight').textContent);
-    const isWeightMatch = bikeWeight >= minWeight && bikeWeight <= maxWeight;
-    const bikeTitle = bike.querySelector('.card-title a').textContent.toLowerCase();
-    const isTitleMatch = bikeTitle.includes(searchInput);
-    const bikeType = bike.querySelector('.type').textContent.trim();
-    const isTypeMatch = selectedType === 'all' || bikeType === selectedType;
-
-    return isYearMatch && isPriceMatch && isSpeedMatch && isWeightMatch && isTitleMatch && isTypeMatch;
-  });
-
-  updateBikeList(filteredBikes);
-}
-// End of function for filters
-
-  // Event listeners for all filters
-  document.getElementById('toggle-filters-button').addEventListener('click', function() {
-    var filters = document.getElementById('price-ranges');
-    if (filters.style.display === "none" || filters.style.display === "") {
-      filters.style.display = "flex";
-      this.textContent = "Hide Filters";
-    } else {
-      filters.style.display = "none";
-      this.textContent = "Show Filters";
-    }
-  });
-
-  function adjustForViewport() {
-    const filters = document.getElementById('price-ranges');
-    const toggleButton = document.getElementById('toggle-filters-button');
-    if (window.innerWidth > 1200) {
-      filters.style.display = 'flex';
-      toggleButton.style.display = 'none';
-    } else {
-      filters.style.display = 'none';
-      toggleButton.style.display = 'inline-block';
-    }
-  }
-  
-  window.addEventListener('load', adjustForViewport);
-  
-  window.addEventListener('resize', adjustForViewport);
-
-  checkboxes.forEach((checkbox) => {
-    checkbox.addEventListener('change', filterBikes);
-  });
-
-  sortLowToHighButton.addEventListener('click', () => {
-      bikes.sort(comparePrices);
-      updateBikeList(bikes);
-  });
-
-  document.getElementById('type-filter').addEventListener('change', filterBikes);
-
-  document.getElementById('title-search').addEventListener('input', filterBikes);
-
-  speedFilterSelect.addEventListener('change', filterBikes);
-
-  weightFilterSelect.addEventListener('change', filterBikes);
-
-  sortHighToLowButton.addEventListener('click', () => {
-      bikes.sort(comparePrices);
-      bikes.reverse();
-      updateBikeList(bikes);
-  });
-
-  yearFilterSelect.addEventListener('change', filterBikes);
-
-  clearFiltersButton.addEventListener('click', () => {
-    weightFilterSelect.value = 'all';
-    yearFilterSelect.value = 'all';
-    speedFilterSelect.value = 'all';
-    document.getElementById('title-search').value = '';
-    document.getElementById('type-filter').value = 'all'
-    checkboxes.forEach((checkbox) => checkbox.checked = false);
-    filterBikes(); 
-  });
-}
-//End of event listeners
-//End of filters
 
 // Cart Elements
 const iconCart = document.querySelector('.icon-cart');
@@ -271,32 +116,17 @@ const body = document.querySelector('body');
 const listCart = document.querySelector('.listCart');
 const iconCartSpan = document.querySelector('.count-items');
 const shadowTwo = document.querySelector('.shadowTwo');
+const params = new URLSearchParams(window.location.search);
+const bikeId = parseInt(params.get('id'));
+const bike = bikes.find(b => b.id === bikeId);
 let isVisible = false;
 
 // Toggle Cart Visibility
-iconCart.addEventListener('click', () => {
-  body.classList.toggle('showCart');
-  shadowTwo.style.display = isVisible ? 'none' : 'block';
-  isVisible = !isVisible;
-});
-
-// Close Cart
-closeCart.addEventListener('click', () => {
-  body.classList.remove('showCart');
-  shadowTwo.style.display = 'none';
-  isVisible = false;
-});
+iconCart.addEventListener('click', toggleCartVisibility);
+closeCart.addEventListener('click', closeCartHandler);
 
 // Populate Bike Details
-  const params = new URLSearchParams(window.location.search);
-  const bikeId = parseInt(params.get('id'));
-  const bike = bikes.find(b => b.id === bikeId);
-
-  if (!isNaN(bikeId)) {
-    fetchBikeDetails(bikeId);
-  } 
-
-  async function fetchBikeDetails(bikeId) {
+async function fetchBikeDetails(bikeId) {
     const dbRef = ref(database, `bikes/${bikeId}`);
     try {
         const snapshot = await get(dbRef);
@@ -310,7 +140,7 @@ closeCart.addEventListener('click', () => {
     } catch (error) {
         console.error("Error fetching bike details: ", error);
     }
-  }  
+}  
 
 function renderBikeDetails(bikeData) {
   const detailsContainer = document.querySelector('.bike-details');
@@ -346,19 +176,16 @@ function renderBikeDetails(bikeData) {
       console.error('Details container not found');
   }
 }
-//End of bike details
+
+if (!isNaN(bikeId)) {
+  fetchBikeDetails(bikeId);
+} 
 
 // Add to Cart Functionality
 const bikeList = document.querySelector('.featured-car-list');
 let cartAdding = [];
 
-bikeList.addEventListener('click', (event) => {
-  const posclick = event.target;
-  if (posclick.classList.contains('btn')) {
-    const productId = posclick.getAttribute('data-id');
-    addToCart(productId);
-  }
-});
+bikeList.addEventListener('click', addToCartHandler);
 
 let currentUserId = null;
 
@@ -420,42 +247,70 @@ const addCartToHTML = () => {
   if (cartAdding.length > 0) {
     cartAdding.forEach(cart => {
       totalQuantity += cart.quantity;
-      const newCart = document.createElement('div');
-      newCart.classList.add('item');
-      newCart.dataset.id = cart.productId;
-      const productInCart = bikes.findIndex((value) => value.id == cart.productId);
-      const info = bikes[productInCart];
-      
-      newCart.innerHTML = `
-        <div class="cartItem">
-          <div class="cartImage">
-            <img src="${info.imageSrc}" alt="">
+      const productInCartIndex = bikes.findIndex((value) => value.id == cart.productId);
+      if (productInCartIndex !== -1) {
+        const productInCart = bikes[productInCartIndex];
+
+        const imageSrc = productInCart.imageSrc ?? '';
+        const title = productInCart.title ?? '';
+        const price = productInCart.price ?? 0;
+
+        const newCart = document.createElement('div');
+        newCart.classList.add('item');
+        newCart.dataset.id = cart.productId;
+        newCart.innerHTML = `
+          <div class="cartItem">
+            <div class="cartImage">
+              <img src="${imageSrc}" alt="">
+            </div>
+            <div class="cartName">${title}</div>
+            <div class="cartPrice">$${price * cart.quantity}</div>
+            <div class="cartQuantity">
+              <span class="minus">-</span>
+              <span>${cart.quantity}</span>
+              <span class="plus">+</span>
+            </div>
           </div>
-          <div class="cartName">${info.title}</div>
-          <div class="cartPrice">$${info.price * cart.quantity}</div>
-          <div class="cartQuantity">
-            <span class="minus">-</span>
-            <span>${cart.quantity}</span>
-            <span class="plus">+</span>
-          </div>
-        </div>
-      `;
-      listCart.appendChild(newCart);
+        `;
+        listCart.appendChild(newCart);
+      }
     });
   }
   iconCartSpan.innerText = totalQuantity;
 };
-//End of HTML for cart
 
 // Modify Cart Item Quantity
-listCart.addEventListener('click', (event) => {
+listCart.addEventListener('click', modifyCartItemQuantity);
+
+//Helper functions
+function toggleCartVisibility() {
+  body.classList.toggle('showCart');
+  shadowTwo.style.display = isVisible ? 'none' : 'block';
+  isVisible = !isVisible;
+}
+
+function closeCartHandler() {
+  body.classList.remove('showCart');
+  shadowTwo.style.display = 'none';
+  isVisible = false;
+}
+
+function addToCartHandler(event) {
+  const posclick = event.target;
+  if (posclick.classList.contains('btn')) {
+    const productId = posclick.getAttribute('data-id');
+    addToCart(productId);
+  }
+}
+
+function modifyCartItemQuantity(event) {
   const clickedElement = event.target;
   if (clickedElement.classList.contains('minus') || clickedElement.classList.contains('plus')) {
     const productId = clickedElement.closest('.item').dataset.id;
     const type = clickedElement.classList.contains('plus') ? 'plus' : 'minus';
     changeQuantity(productId, type);
   }
-});
+}
 
 const changeQuantity = (productId, type) => {
   const quantityChecker = cartAdding.findIndex((value) => value.productId == productId);
@@ -476,6 +331,146 @@ const changeQuantity = (productId, type) => {
   addCartToHTML();
   saveCartToFirestore(cartAdding, currentUserId);
 };
-// End of Cart Functionality
 }
 ProductsFirebase();
+
+
+//Filters
+function initFilters() {
+  const speedFilterSelect = document.getElementById('speed-filter');
+  const weightFilterSelect = document.getElementById('weight-filter');
+  const bikeList = document.querySelector('.featured-car-list');
+  const clearFiltersButton = document.getElementById('clear-filters-button');
+  const sortLowToHighButton = document.getElementById('sort-low-to-high');
+  const sortHighToLowButton = document.getElementById('sort-high-to-low');
+  const yearFilterSelect = document.getElementById('year-filter');
+  const checkboxes = document.querySelectorAll('#price-ranges input[type="checkbox"]');
+  const toggleFiltersButton = document.getElementById('toggle-filters-button');
+
+  const bikes = Array.from(bikeList.children);
+  const originalBikesOrder = Array.from(bikes);
+
+  // Price range for checkboxes
+  const priceRanges = {
+      'range-0-400': [0, 400],
+      'range-400-600': [401, 599],
+      'range-600-800': [600, 800],
+  };
+
+  // Compare bike prices
+  const comparePrices = (a, b) => {
+      const priceA = parseFloat(a.querySelector('.card-price strong').textContent.replace('$', ''));
+      const priceB = parseFloat(b.querySelector('.card-price strong').textContent.replace('$', ''));
+      return priceA - priceB;
+  };
+
+  // Update bike list
+  const updateBikeList = (bikeArray) => {
+    bikeList.innerHTML = ''; 
+    if (bikeArray.length > 0) {
+        bikeArray.forEach((bike) => {
+            bikeList.appendChild(bike);
+        });
+    } else {
+        bikeList.innerHTML = ' <img class = "notFound" src="https://firebasestorage.googleapis.com/v0/b/ecobike-bb6cc.appspot.com/o/itemNotFound.png?alt=media&token=e17013a0-ae20-48d1-ba37-6784199a0c64">';
+    }
+  };
+
+// Function for working filters
+function filterBikes() {
+  const searchInput = document.getElementById('title-search').value.toLowerCase();
+  const selectedType = document.getElementById('type-filter').value;
+  const selectedSpeedRange = speedFilterSelect.value;
+  const selectedWeightRange = weightFilterSelect.value;
+  const selectedYear = yearFilterSelect.value;
+  const selectedPriceRanges = [];
+
+  let [minSpeed, maxSpeed] = selectedSpeedRange !== 'all' ? selectedSpeedRange.split('-').map(Number) : [0, Infinity];
+  let [minWeight, maxWeight] = selectedWeightRange !== 'all' ? selectedWeightRange.split('-').map(Number) : [0, Infinity];
+
+  checkboxes.forEach((checkbox) => {
+    if (checkbox.checked) {
+      const rangeId = checkbox.id;
+      selectedPriceRanges.push(priceRanges[rangeId]);
+    }
+  });
+
+  // Connected filters
+  const filteredBikes = originalBikesOrder.filter((bike) => {
+    
+    const bikePrice = parseFloat(bike.querySelector('.card-price strong').textContent.replace('$', ''));
+    const bikeYear = bike.querySelector('.year').getAttribute('value');
+    const bikeSpeed = parseFloat(bike.querySelector('.speed').textContent);
+    const bikeWeight = parseFloat(bike.querySelector('.weight').textContent);
+    const bikeTitle = bike.querySelector('.card-title a').textContent.toLowerCase();
+    const bikeType = bike.querySelector('.type').textContent.trim();
+
+    const isSpeedMatch = bikeSpeed >= minSpeed && bikeSpeed <= maxSpeed;
+    const isYearMatch = selectedYear === 'all' || bikeYear === selectedYear;
+    const isPriceMatch = selectedPriceRanges.length === 0 || selectedPriceRanges.some(([minPrice, maxPrice]) => bikePrice >= minPrice && bikePrice <= maxPrice);
+    const isWeightMatch = bikeWeight >= minWeight && bikeWeight <= maxWeight;
+    const isTitleMatch = bikeTitle.includes(searchInput);
+    const isTypeMatch = selectedType === 'all' || bikeType === selectedType;
+
+    return isYearMatch && isPriceMatch && isSpeedMatch && isWeightMatch && isTitleMatch && isTypeMatch;
+  });
+
+  updateBikeList(filteredBikes);
+}
+
+  // Event listeners for all filters
+  toggleFiltersButton.addEventListener('click', function() {
+    var filters = document.getElementById('price-ranges');
+    if (filters.style.display === "none" || filters.style.display === "") {
+      filters.style.display = "flex";
+      this.textContent = "Hide Filters";
+    } else {
+      filters.style.display = "none";
+      this.textContent = "Show Filters";
+    }
+  });
+
+  checkboxes.forEach((checkbox) => checkbox.addEventListener('change', filterBikes));
+  sortLowToHighButton.addEventListener('click', () => {
+    bikes.sort(comparePrices);
+    updateBikeList(bikes);
+  });
+
+  sortHighToLowButton.addEventListener('click', () => {
+    bikes.sort(comparePrices).reverse();
+    updateBikeList(bikes);
+  });
+
+  document.getElementById('type-filter').addEventListener('change', filterBikes);
+  document.getElementById('title-search').addEventListener('input', filterBikes);
+
+  speedFilterSelect.addEventListener('change', filterBikes);
+  weightFilterSelect.addEventListener('change', filterBikes);
+  yearFilterSelect.addEventListener('change', filterBikes);
+  clearFiltersButton.addEventListener('click', () => {
+    weightFilterSelect.value = 'all';
+    yearFilterSelect.value = 'all';
+    speedFilterSelect.value = 'all';
+    document.getElementById('title-search').value = '';
+    document.getElementById('type-filter').value = 'all';
+    checkboxes.forEach((checkbox) => checkbox.checked = false);
+    filterBikes(); 
+  });
+
+    // Adjust filters for different screens
+  function adjustForViewport() {
+    const filters = document.getElementById('price-ranges');
+    const toggleButton = document.getElementById('toggle-filters-button');
+    if (window.innerWidth > 1200) {
+      filters.style.display = 'flex';
+      toggleButton.style.display = 'none';
+    } else {
+      filters.style.display = 'none';
+      toggleButton.style.display = 'inline-block';
+    }
+  }
+  
+  window.addEventListener('load', adjustForViewport);
+  
+  window.addEventListener('resize', adjustForViewport);
+}
